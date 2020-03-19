4eed781bc3e0f3389f5306061ac2590abf64506a
Gary Little <gary.j.little@gmail.com>
2020-03-19 19:05:26 -0400

    Initial commit, using getopts in a bash function

---
+++ getops-function.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+# Example of using arguments with optargs
+# See also  http://www.tldp.org/LDP/abs/html/internal.html#EX33
+
+NO_ARGS=0
+let VERBOSE=0
+DEST=""
+SOURCE=""
+
+function process_cli_args() { 
+local OPTIND
+if [ $# -eq $NO_ARGS ] ; then
+    echo "No arguments"
+else 
+ 
+
+# Use the "getopts <stringlist>"" to set variables based on the arguments passed in.
+# Some options have an argument e.g. -f <filename> and are specified with a colon
+# Some options do not, and are more like a switch e.g.  Enable debugging with -d
+# s = source pathname
+# d = destination pathname
+# v = Enable verbose.
+
+while getopts "s:d:v" Option
+do
+    case $Option in
+        d   )   DEST=$OPTARG ;;
+        s   )   SOURCE=$OPTARG ;;
+        v   )   VERBOSE=1 ;;
+    esac
+done
+fi
+}
+
+process_cli_args "$@"
+
+
+if [ ! -z $DEST  ]; then echo "The Destination is $DEST" ; fi
+if [ ! -z $SOURCE ]; then echo "The Source is $SOURCE" ; fi
+if [  $VERBOSE -gt 0 ]; then echo "Verboseness is $VERBOSE" ; fi
